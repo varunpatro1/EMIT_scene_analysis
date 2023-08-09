8cfe7b49fee563e76d5c8912550aa072821aeeb0
Patro <vpatro@ndc.nasa.gov>
2023-08-09 14:43:55 -0700

    reorganized directory and plotted elev and sza global variance of lhs samples

--- .ipynb_checkpoints/spatial_plot-checkpoint.py
+++ .ipynb_checkpoints/spatial_plot-checkpoint.py
@@ -6,8 +6,8 @@ from geopandas import GeoDataFrame
 from shapely.geometry import Point
 
 def spatial_plt(df):
-    geometry = [Point(xy) for xy in zip(samples['lat'], samples['lon'])]
-    gdf = GeoDataFrame(samples, geometry = geometry)
+    geometry = [Point(xy) for xy in zip(df['lat'], df['lon'])]
+    gdf = GeoDataFrame(df, geometry = geometry)
 
     world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
     gdf.plot(ax = world.plot(figsize = (15,15)), marker = 'o', color = 'red', markersize = 3);
@@ -28,7 +28,7 @@ def metric_variance_plt(df, metric):
         df.plot.scatter(x="lat", y="lon", s = 5, c=df[df_metric], colormap="plasma", title= title, ax=ax[i])
 
 
-def elev_zen_variance_plt(df, metric):
+def elev_zen_variance_plt(df):
     title_elev = 'Global Map of Elevation Variance'
     title_zen = 'Global Map of Zenith Angle Variance'
     fig, ax = plt.subplots(figsize=(20,16), ncols = 1, nrows = 2, gridspec_kw = None)
@@ -39,7 +39,7 @@ def elev_zen_variance_plt(df, metric):
         countries.plot(color="lightblue", ax = ax[i])
         
     # plot points
-    df.plot.scatter(x="lat", y="lon", s = 5, c=df['elev med'], colormap="plasma", title= title_elev, ax=ax[0])
-    df.plot.scatter(x="lat", y="lon", s = 5, c=df['zen'], colormap="plasma", title= title_zen, ax=ax[1])
+    df.plot.scatter(x="lat", y="lon", s = 8, c=df['elev med'], colormap="plasma", title= title_elev, ax=ax[0])
+    df.plot.scatter(x="lat", y="lon", s = 8, c=df['zen'], colormap="plasma", title= title_zen, ax=ax[1])
 
     
