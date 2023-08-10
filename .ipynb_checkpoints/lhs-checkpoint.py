c7b34ea637e2f27903c216f5ada17cc95f151db5
Patro <vpatro@ndc.nasa.gov>
2023-08-09 18:45:21 -0700

    plotted correlation and covariance matrices

--- .ipynb_checkpoints/lhs-checkpoint.py
+++ .ipynb_checkpoints/lhs-checkpoint.py
@@ -38,7 +38,7 @@ def LatinHypercubeSample2(df):
     wv = df['wv range']
     sza = df['zen']
     
-    bins = 30
+    bins = 35
     # building intervals
     wv_range = np.linspace(np.min(wv),np.max(wv), bins)
     sza_range = np.linspace(np.min(sza),np.max(sza), bins)
@@ -52,6 +52,33 @@ def LatinHypercubeSample2(df):
         for _sza in range(len(sza_range) - 1):
             subset = (wv > wv_range[_w]) & (wv <= wv_range[_w + 1]) & \
             (sza > sza_range[_sza]) & (sza <= sza_range[_sza + 1])
+            perm = np.random.permutation(np.sum(subset))
+    
+            samples = np.where(subset)[0][perm[:samples_per_space]]
+            sample_set.extend(samples.tolist())
+
+    return sample_set
+
+def LatinHypercubeSample3(df):
+    
+    np.random.seed(13)
+    
+    elev = df['elev med']
+    sza = df['zen']
+    
+    bins = 31
+    # building intervals
+    e_range = np.linspace(np.min(elev),np.max(elev), bins)
+    sza_range = np.linspace(np.min(sza),np.max(sza), bins)
+    
+    
+    samples_per_space = 1
+    print(f'num samples to build: {(bins-1)**2*samples_per_space}')
+    
+    sample_set = []
+    for _e in range(len(e_range) - 1):
+        for _sza in range(len(sza_range) - 1):
+            subset = (elev > e_range[_e]) & (elev <= e_range[_e + 1]) & (sza > sza_range[_sza]) & (sza <= sza_range[_sza + 1])
             perm = np.random.permutation(np.sum(subset))
     
             samples = np.where(subset)[0][perm[:samples_per_space]]
